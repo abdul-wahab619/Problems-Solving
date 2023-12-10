f63e006032b036f19a903e3886225ae5df16fb4f
Abdul Wahab <awminhas619@gmail.com>
2023-12-10 15:24:46 +0500

    escape_sequence

---
+++ Intermediate/escape_sequence.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+int main ()
+{
+    cout << "1\n23\n456\n78910";
+    cout << "\n1\t2\t3";
+    cout << "\nAbdul\fWahab";
+    cout << "\nAbdul\bWahab";
+    cout << "\nAbdul\aWahab";
+    cout << "\nAbdul\rWahab";
+    cout << "\nAbdul\'Wahab";
+    cout << "\nAbdul\"Wahab";
+    cout << "\nAbdul\\Wahab";
+
+}
