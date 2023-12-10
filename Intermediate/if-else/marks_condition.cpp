48433e5d4753ae627d18f949931d74e86994788a
Abdul Wahab <awminhas619@gmail.com>
2023-12-10 17:09:58 +0500

    marks_condition

---
+++ Intermediate/if-else/marks_condition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int marks;
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    if (marks >= 40) {
+        cout << "Congrats your are passed!" << endl;
+    }
+}
