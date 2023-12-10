3993d3ab7c3f30dbc8fa23065735b2662e231cfc
Abdul Wahab <awminhas619@gmail.com>
2023-12-10 15:05:39 +0500

    type_casting

---
+++ Basics/type_casting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+int main ()
+{
+    float a, b;
+    int c;
+    a = 10.3;
+    b = 5.2;
+    c = (int)a % (int)b;
+    cout << "Type Casting result is: " << c;
+    
+}
