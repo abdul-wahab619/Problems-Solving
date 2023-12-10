1e1d660011886079facb21a5b37d6d900667bf38
Abdul Wahab <awminhas619@gmail.com>
2023-12-10 17:40:44 +0500

    check-even-odd

---
+++ Intermediate/if-else/check-even-odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num % 2 == 0) {
+        cout << "It is Even number." << endl;
+    }
+    else {
+        cout << "It is odd number." << endl;
+    }
+    
+}
