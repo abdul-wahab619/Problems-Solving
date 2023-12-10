1e8f8e9987bf5590d5c456e0f3ce36bbfa1b18ed
Abdul Wahab <awminhas619@gmail.com>
2023-12-10 15:18:14 +0500

    add

---
+++ Intermediate/add.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; 
+int main ()
+{
+    float num1, num2, sum;
+    sum = num1 + num2;
+
+    cout << "Result of " << num1 << " and " << num2 << " is: " << sum << endl;
+}
