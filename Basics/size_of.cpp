2ea9b775489821c460536675e8a4e755ea22c288
Abdul Wahab <awminhas619@gmail.com>
2023-12-10 15:08:56 +0500

    size_of

---
+++ Basics/size_of.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+int main ()
+{
+    cout << "Size of Char: " << sizeof(char) << endl;
+    cout << "Size of Int: " << sizeof(int) << endl;
+    cout << "Size of Float: " << sizeof(float) << endl;
+    cout << "Size of Long: " << sizeof(long) << endl;
+    cout << "Size of Double: " << sizeof(double) << endl;
+    cout << "Size of Long Double: " << sizeof(long long) << endl;
+
+}
