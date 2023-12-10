4984faff7f082fb4c1b620c25279aff4c16aa063
Abdul Wahab <awminhas619@gmail.com>
2023-12-10 14:25:18 +0500

    assign_values

---
+++ Basics/assign_values.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+int main ()
+{
+    char ch1, ch2;
+    ch1 = 'A';
+    ch2 = 'W';
+    cout << "Values of ch1 and ch2 are: " << ch1 << " " << ch2;
+    return (0);
+}
