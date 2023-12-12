3218d5b852539561bc4b426c347d9e27d72e9189
Abdul Wahab <awminhas619@gmail.com>
2023-12-13 03:12:46 +0500

    simplePrints

---
+++ Intermediate/Loop/While/simplePrints.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 1;
+    while (n<=5)
+    {
+        cout << n <<"- Hi Abdul Wahab.Tum has ho!"<<endl;
+        n++;
+    }
+    return (0);
+}
