0ca8c7c6b22177a3d28e472ea3be875dfcfa13a2
Abdul Wahab <awminhas619@gmail.com>
2024-01-01 22:10:59 +0500

    take-2-inputs-and-perform-operations

---
+++ Coding-Problem-Solved/take-2-inputs-and-perform-operations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "\n"
+         << a + b;
+    cout << "\n"
+         << a * b;
+    cout << "\n"
+         << a - b;
+}
