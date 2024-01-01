84366ead11a4d30383f793a358bb0f6776b9f4ce
Abdul Wahab <awminhas619@gmail.com>
2024-01-01 20:41:30 +0500

    check-prime-or-not

---
+++ Coding-Problem-Solved/check-prime-or-not.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    bool check = false;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            check = true;
+        }
+    }
+
+    if (check == false)
+        cout << "Given number is a prime number." << endl;
+    else
+        cout << "Given number is not a prime number." << endl;
+}
